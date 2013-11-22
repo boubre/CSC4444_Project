97926c6ae83abe5782b4fc47c7808c6678ecfea6
dcastl2 <dcastl2@tigers.lsu.edu>
2013-11-22 16:44:12 -0600

    Added hypo0 (our base model) and updated main.R (experimental testbed).

--- hypo0.R
+++ hypo0.R
@@ -28,10 +28,18 @@ f <- as.formula(f);
 T <- vector(mode="list", length=N-1);
 
 
-# Loop over data
+# Need at least 4 training entries for Naive Bayes
 init=4;
 i=init;
+
+
+# Accuracy; how many correctly classified
 bayes_hits <- 0;
+knn_hits   <- 0;
+svm_hits   <- 0;
+
+
+# Loop over data
 while (i < N) {
 
 
@@ -41,40 +49,28 @@ while (i < N) {
   ty <- td[1:i, D];
   ty <- as.factor(ty);
 
-
-  # Append last class value to training data
-  tx[1,D] <- td[1,D];
-  if (i > 1) {
-    # Time series
-    T[[i]] <- ts(td);
-    for (j in 2:i) {
-      tx[j,D] <- td[j-1,D];
-    }
-  }
-  
-
   # Target
   vd <- d[i+1,  ];
   vx <- d[i+1,-D];
   vy <- d[i+1, D];
 
-
-  # Append last class to validation entry 
-  vx[1, D] <- d[i, D];
-
-
   # Classifier models
   ann   <- neuralnet(formula=f, data=td, threshold=.5);
   bayes <- naiveBayes(x=tx, y=ty);
   knn   <- knn(td, vd, cl=ty);
-  #svm   <- svm(tx, ty, gamma=10, type="C-classification");
+  svm   <- svm(tx, ty, gamma=10, type="C-classification");
 
 
   # Increment _hits for each correct classification
   if (predict(bayes, vx)==vy) {
     bayes_hits = bayes_hits + 1;
   }
-
+  if (knn==vy) {
+    knn_hits = knn_hits + 1;
+  }
+  if (predict(svm, vx)==vy) {
+    svm_hits = svm_hits + 1;
+  }
 
   i=i+1;
 
@@ -82,12 +78,6 @@ while (i < N) {
 
 Nval <- N-init;
 print(bayes_hits/Nval);
+print(knn_hits/Nval);
+print(svm_hits/Nval);
 
-
-# Animation of time series
-#saveGIF({
-#    ani.options(nmax = 22, loop=1, interval=.1)
-#    for (i in 1:23) {
-#      plot(T[[i]]);
-#    }
-#}, interval = 0.05, movie.name = "bm_demo.gif", ani.width = 600, ani.height = 600)
